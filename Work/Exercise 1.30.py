ca732028f7e438f9b6e4150ebb291c0904867150
Amir Khoshbooie <ahkhoshbooie@mun.ca>
2020-07-17 23:38:40 -0230

    1.30

---
+++ Work/Exercise 1.30.py
@@ -0,0 +1,19 @@
+#Exercise 1.30
+
+def portfolio_cost(filename):
+
+total_cost = 0.0
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost += nshares * price
+
+print('Total cost', total_cost)
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', total_cost)
+
