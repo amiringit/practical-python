a782e6ce2b6c33029ca0ffd75cae9e9ccf4c069f
Amir Khoshbooie <ahkhoshbooie@mun.ca>
2020-07-20 23:52:41 -0230

    I tried to use f

--- Work/Exercise 1.30.py
+++ Work/Exercise 1.30.py
@@ -1,15 +1,13 @@
 #Exercise 1.30
 
 def portfolio_cost(filename):
-    total_cost = 0.0
-    with open('Data/portfolio.csv', 'rt') as f:
-        headers = next(f)
-        for line in f:
+    import csv
+    with open(filename, 'rt') as input:
+        headers = next(input)
+        total_cost = 0
+        for line in input:
             row = line.split(',')
-            nshares = int(row[1])
-            price = float(row[2])
-            total_cost = total_cost + nshares * price
-    print('Total cost', total_cost)    
-cost = portfolio_cost('Data/portfolio.csv')
-print('Total cost:', total_cost)
+            total_cost += (int(row[1]) * float(row[2]))
+        print('Total cost:', total_cost)
+
 
